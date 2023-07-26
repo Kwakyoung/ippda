4991d4c1fe2cdb786002ae843f12e1fcf4146132
joeunpyeong <128439456+joeunpyeong@users.noreply.github.com>
2023-07-26 13:37:51 +0900

    .

--- app/src/main/java/com/example/ipdda/profile/ProfileFragment.java
+++ app/src/main/java/com/example/ipdda/profile/ProfileFragment.java
@@ -9,15 +9,17 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.ipdda.R;
+import com.example.ipdda.databinding.FragmentHomeBinding;
+import com.example.ipdda.databinding.FragmentProfileBinding;
 
 public class ProfileFragment extends Fragment {
-
+    FragmentProfileBinding binding;
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        binding = FragmentProfileBinding.inflate(inflater, container, false);
+        return binding.getRoot();
     }
 }

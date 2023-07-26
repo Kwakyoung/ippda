1964b29da8a8ff95d636a2ba714fb49a54b6a505
Kwakyoung <128439473+Kwakyoung@users.noreply.github.com>
2023-07-26 13:42:37 +0900

    0726

--- app/src/main/java/com/example/ipdda/LoginActivity.java
+++ app/src/main/java/com/example/ipdda/LoginActivity.java
@@ -1,21 +1,25 @@
 package com.example.ipdda;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 
 import android.content.Intent;
 import android.os.Bundle;
 
 import com.example.ipdda.databinding.ActivityLoginBinding;
+import com.example.ipdda.home.HomeFragment;
 
 public class LoginActivity extends AppCompatActivity {
 
     ActivityLoginBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         binding = ActivityLoginBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        FragmentManager manager = getSupportFragmentManager();
+
+
         binding.btnLogin.setOnClickListener(v -> {
             Intent intent = new Intent(this, MainActivity.class);
             startActivity(intent);

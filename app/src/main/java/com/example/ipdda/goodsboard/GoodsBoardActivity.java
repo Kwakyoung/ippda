42f4d653e87b470bd125202f2c931478884e5e02
bongbong2 <106155992+bongbongbon@users.noreply.github.com>
2023-08-09 16:30:31 +0900

    230809

--- app/src/main/java/com/example/ipdda/goodsboard/GoodsBoardActivity.java
+++ app/src/main/java/com/example/ipdda/goodsboard/GoodsBoardActivity.java
@@ -1,17 +1,23 @@
 package com.example.ipdda.goodsboard;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
+import com.example.ipdda.MainActivity;
 import com.example.ipdda.R;
 import com.example.ipdda.common.CommonConn;
 import com.example.ipdda.databinding.ActivityGoodsBoardBinding;
 import com.example.ipdda.home.GoodsVO;
+import com.example.ipdda.home.HomeFragment;
 import com.example.ipdda.home.HomeGoodsRecommendAdapter;
+import com.example.ipdda.profile.SubActivity;
+import com.example.ipdda.search.SearchFragment;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
@@ -20,6 +26,8 @@ import java.sql.Connection;
 import java.util.ArrayList;
 
 public class GoodsBoardActivity extends AppCompatActivity {
+
+
 
     ActivityGoodsBoardBinding binding;
     @Override
@@ -31,12 +39,29 @@ public class GoodsBoardActivity extends AppCompatActivity {
         binding.recvReview.setAdapter(new GoodsBoardReviewAdapter(GetGoodsBoardReview(),this));
         binding.recvReview.setLayoutManager(new LinearLayoutManager(this));
 
+        binding.imgvHome.setOnClickListener(v -> {
+                Intent intent = new Intent(this, MainActivity.class);
+                startActivity(intent);
+        });
+
+
+        binding.imgvSearch.setOnClickListener(v -> {
+            SearchFragment searchFragment = new SearchFragment();
+            searchFragment.show(getSupportFragmentManager(), "search_dialog");
+        });
+
+        binding.imgvMybag.setOnClickListener(v -> {
+            Intent intent = new Intent(this, SubActivity.class);
+            intent.putExtra("key", 0);
+            startActivity(intent);
+        });
+
         binding.imgvBack.setOnClickListener(v -> {
             finish();
         });
 
         int goods_no = getIntent().getIntExtra("goods_no", 0);
-        Log.d("goods_no", "onCreate: " + goods_no);//goods/goodsboard
+        Log.d("goods_no", "onCreate: " + goods_no);
         CommonConn conn = new CommonConn(this, "goods/goodsboard");
         conn.addParamMap("goods_no" , goods_no);
         conn.onExcute((isResult, data) -> {
@@ -44,16 +69,33 @@ public class GoodsBoardActivity extends AppCompatActivity {
             GoodsVO goodsVO = arrayList.get(0);
 
             String goodsName = goodsVO.getGoods_name();
-            String goodsPrice = goodsVO.getGoods_price()+"";
+            int goodsPrice = goodsVO.getGoods_price();
+            int SalePercent = goodsVO.getGoods_sale_percent();
+
             String storeName = goodsVO.getStore_name()+"";
             String starCnt = goodsVO.getGoods_star()+"";
+            String goodsContext = goodsVO.getGoods_info()+"";
+
+
+
+            if(SalePercent == 0){
+                binding.tvGoodsPrice.setText(goodsPrice+"원");
+                binding.tvGoodsOriginalPrice.setVisibility(View.GONE);
+                binding.tvOriginalPrice.setVisibility(View.GONE);
+                binding.tvSalePercent.setVisibility(View.GONE);
+                binding.tvSale.setVisibility(View.GONE);
+            }else{
+                int goodsSalePrice = goodsPrice/(100/SalePercent);
+                binding.tvGoodsPrice.setText(goodsSalePrice+"원");
+                binding.tvGoodsOriginalPrice.setText(goodsPrice+"원");
+            }
 
 
             binding.tvStoreName.setText(storeName);
             binding.tvStarCnt.setText(starCnt);
             binding.tvGoodsName.setText(goodsName);
-            binding.tvGoodsPrice.setText(goodsPrice);
-
+            binding.tvGoodsContext.setText(goodsContext);
+            binding.tvSalePercent.setText(SalePercent+"");
         });
 //
 

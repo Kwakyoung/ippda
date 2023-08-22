3cf31591a53650666a1519585d9622d9a67cef5e
Kwakyoung <128439473+Kwakyoung@users.noreply.github.com>
2023-08-22 18:06:48 +0900

    update

---
+++ ippda_web/src/main/java/com/hanul/cloude/MemberController.java
@@ -0,0 +1,52 @@
+package com.hanul.cloude;
+
+import java.io.FileInputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.regex.Pattern;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.apache.ibatis.session.SqlSession;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.messaging.FirebaseMessaging;
+import com.google.firebase.messaging.Message;
+import com.google.firebase.messaging.Notification;
+
+@Controller
+public class MemberController {
+	
+	@Autowired @Qualifier("ippda") SqlSession sql;
+	
+	// 로그인화면 요청
+		@RequestMapping("/login")
+		public String login(HttpSession session) {
+			session.setAttribute("category", "login");
+			return "default/member/login";
+		}
+		
+		
+		// 회원가입 화면
+		@RequestMapping("/register")
+		public String register() {
+			return "default/member/register";
+		}
+		
+		
+}
---
+++ ippda_web/src/main/java/member/MemberDAO.java
@@ -0,0 +1,62 @@
+package member;
+
+import java.util.HashMap;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.google.gson.Gson;
+
+
+
+
+
+@Repository
+public class MemberDAO {
+	
+	
+	@Autowired @Qualifier("ippda") SqlSession sql;
+	public MemberVO login(HashMap<String , String> params) {
+		MemberVO vo = sql.selectOne("member.login", params);
+		return vo;
+}
+	
+//	public int insert(@RequestParam String member_id , ) {
+//		HashMap<Stirng, Object> params = new HashMap<String, Object>();
+//		params
+//		
+//		return sql.insert("member.join",vo);
+//	}
+	
+	
+	public MemberVO idcheck(String member_id) {
+		return sql.selectOne("member.idcheck",member_id);
+	}
+	
+	public MemberVO	check(MemberVO vo) {
+		return sql.selectOne("member.check",vo);
+	}
+	
+	
+	
+	public MemberVO	order(MemberVO vo) {
+		return sql.selectOne("member.order",vo);
+	}
+	
+
+	
+	public MemberVO findid(MemberVO vo) {
+		return sql.selectOne("member.findid",vo);
+	}
+	
+	
+	public int resetPw(MemberVO vo) {
+		return sql.update("member.resetPw", vo);
+	}
+	
+	
+	
+}

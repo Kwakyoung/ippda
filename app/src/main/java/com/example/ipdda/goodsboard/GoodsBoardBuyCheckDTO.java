1ec9b187c02853a0434fc02bc4c85afc367e4eb7
joeunpyeong <128439456+joeunpyeong@users.noreply.github.com>
2023-08-23 09:50:58 +0900

    2023-08-23

---
+++ app/src/main/java/com/example/ipdda/goodsboard/GoodsBoardBuyCheckDTO.java
@@ -0,0 +1,30 @@
+package com.example.ipdda.goodsboard;
+
+public class GoodsBoardBuyCheckDTO {
+     private String check_goods_name;
+     private int check_goods_cnt, check_goods_price;
+
+    public String getCheck_goods_name() {
+        return check_goods_name;
+    }
+
+    public void setCheck_goods_name(String check_goods_name) {
+        this.check_goods_name = check_goods_name;
+    }
+
+    public int getCheck_goods_cnt() {
+        return check_goods_cnt;
+    }
+
+    public void setCheck_goods_cnt(int check_goods_cnt) {
+        this.check_goods_cnt = check_goods_cnt;
+    }
+
+    public int getCheck_goods_price() {
+        return check_goods_price;
+    }
+
+    public void setCheck_goods_price(int check_goods_price) {
+        this.check_goods_price = check_goods_price;
+    }
+}
